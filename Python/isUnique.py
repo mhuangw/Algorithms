5a30733d85691dc70e858e5a8b90b736ac08b908
Michael Huang <mwhuang2@gmail.com>
2017-01-07 12:18:44 -0600

    added isUnique.py

---
+++ Python/isUnique.py
@@ -0,0 +1,14 @@
+# Checks if a string has all unique characters
+def isUnique(str):
+	if str is None:
+		return True
+	# String is not unique if length exceeds number of ASCII characters
+	if len(str) > 128:
+		return False
+	# Dictionary to check occurrence of each ASCII character in the string
+	chars = {}
+	for char in str:
+		if char in chars:
+			return False
+		chars[char] = True
+	return True
