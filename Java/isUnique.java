d0d15bb2706ae3a4fbac1559048a0ce06279123f
Michael Huang <mwhuang2@gmail.com>
2017-01-07 12:01:48 -0600

    added isUnique.java

---
+++ Java/isUnique.java
@@ -0,0 +1,18 @@
+static boolean isUnique(String str) {
+	if(str == null) {
+		return null;
+	}
+	// String is not unique if length exceeds maximum ASCII count
+	if(str.length() > 128) {
+		return false;
+	}
+	boolean[] chars = new boolean[128];
+	for(int i = 0; i < str.length(); i++) {
+		int val = str.charAt(i);
+		if(chars[val]) {
+			return false;
+		}
+		chars[va] = true;
+	}
+	return true;
+}
