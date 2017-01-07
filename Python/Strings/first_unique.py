955ac5449656109fef7000fd3624827da0a4eb50
Michael Huang <mwhuang2@gmail.com>
2017-01-07 12:34:07 -0600

    Add first_unique.py

---
+++ Python/Strings/first_unique.py
@@ -0,0 +1,16 @@
+""" Returns the first unique (non-recurring) character in a string
+	If no unique characters are present, returns None
+	"""
+def first_unique(str):
+	if str is None:
+		return None
+	charCount = {}
+	for x in str: 
+		if x in charCount:
+			charCount[x] += 1
+		else:
+			charCount[x] = 1
+	for y in str:
+		if charCount[y] = 1:
+			return y
+	return None
