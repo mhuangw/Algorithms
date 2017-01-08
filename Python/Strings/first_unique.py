0773281339bc7e721b85e00ff09f087958d341d6
Michael Huang <mhuangw@gmail.com>
2017-01-07 21:15:29 -0600

    Update first_unique.py

--- Python/Strings/first_unique.py
+++ Python/Strings/first_unique.py
@@ -2,15 +2,15 @@
 	If no unique characters are present, returns None
 """
 def first_unique(str):
-	if str is None:
-		return None
-	charCount = {}
-	for x in str: 
-		if x in charCount:
-			charCount[x] += 1
-		else:
-			charCount[x] = 1
-	for y in str:
-		if charCount[y] == 1:
-			return y
-	return None
+  if str is None:
+    return None
+  charCount = {}
+  for x in str:
+    if x in charCount:
+      charCount[x] += 1
+    else:
+      charCount[x] = 1
+  for y in str:
+    if charCount[y] == 1:
+      return y
+  return None
